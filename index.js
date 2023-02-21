8bea7a330564dc97077988b5221d6de5238d6dc7
IsiteYves <yvesisite@gmail.com>
2023-02-21 20:37:30 +0200

    backup: to original needed

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function add(num1, num2) {
 // main function
 function main() {
   // get the arguments passed through command line
-  const numbers = process.argv.slice(3);
+  const numbers = process.argv.slice(2);
 
   // check if both numbers are passed
   if (numbers.length !== 2) {
@@ -17,12 +17,7 @@ function main() {
   // add two numbers
   const result = add(numbers[0], numbers[1]);
   console.log(
-    "The result of adding " +
-      numbers[0] +
-      " and " +
-      numbers[1] +
-      " is " +
-      result
+    `The result of adding ${numbers[0]} and ${numbers[1]} is ${result}`
   );
 }
 
